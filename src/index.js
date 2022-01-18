67cda73d5e1aab0f5dc4fad55f6c8bb65609fefe
Phi <34006171@parisnanterre.fr>
2022-01-18 18:33:26 +0100

    Fix bug and add components

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import './index.css';
 import App from './App';
 
 const DATA = [
-    { id: "todo-0", name: "Eat", completed: true },
-    { id: "todo-1", name: "Sleep", completed: false },
-    { id: "todo-2", name: "Repeat", completed: false }
-  ];
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false }
+];
+
   
 //ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<App tasks="DATA" />, document.getElementById('root'));
+ReactDOM.render(<App tasks={DATA} />, document.getElementById('root'));
